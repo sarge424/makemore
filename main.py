128bbf3e690d6bd7e3e28c48700e8f26ca0f9a2d
Alex Dawson <arjunsivakumar2@gmail.com>
2023-03-01 20:59:20 +0530

    Basic MLP done

--- main.py
+++ main.py
@@ -15,20 +15,41 @@ itos = {i:s for s,i in stoi.items()}
 
 #create training sets
 X, Y = [], []
-block_size = 5
+block_size = 3
 
-for word in names[:3]:
+for word in names[:5]:
     context = [0] * block_size
-    print(word)
     for ch in word + '.':
         X.append(context)
         Y.append(stoi[ch])
-        
-        print(''.join(itos[x] for x in context), '->', ch)
         context = context[1:] + [stoi[ch]]
         
+n_ex = len(Y)
 X = torch.tensor(X)
 Y = torch.tensor(Y)
 
-#build lookup table
-C = torch.randn((27, 2))
+g = torch.Generator().manual_seed(2147483647)
+C = torch.randn((27, 2), generator=g)
+W1 = torch.randn((6, 100), generator=g)
+b1 = torch.randn(100, generator=g)
+W2 = torch.randn((100, 27), generator=g)
+b2 = torch.randn(27, generator=g)
+
+parameters = [C, W1, b1, W2, b2]
+for p in parameters:
+    p.requires_grad = True
+
+for _ in range(20):
+    #forward pass
+    emb = C[X]
+    h = torch.tanh(emb.view(-1, 6) @ W1 + b1)
+    logits = h @ W2 + b2
+    loss = F.cross_entropy(logits, Y)
+    print(loss.item())
+
+    #backward pass
+    for p in parameters:
+        p.grad = None
+    loss.backward()
+    for p in parameters:
+        p.data += -0.1 * p.grad
